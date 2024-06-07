b6711984b4fd30e1a59bae595e860d13130621d6
zharrow <florent.detres@protonmail.com>
2024-06-07 08:44:42 +0200

    feat: store data to csv

--- game/player.py
+++ game/player.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Player:
     def __init__(self, name):
@@ -14,24 +15,37 @@ class Player:
 
 class HumanPlayer(Player):
     def do_choice(self):
-        choice = int(input(f"{self.name}, enter your choice \n1.Rock\n2.Paper\n3.Scissors\n: "))
-        while choice not in [1, 2, 3]:
-            choice = int(input("Invalid choice.\n1.Rock\n2.Paper\n3.Scissors\n: "))
+        user_input = int(input(f"{self.name}, enter your choice: Rock(1) Paper(2) Scissors(3). Press (9) to quit: "))
+        while user_input not in [1, 2, 3, 9]:
+            user_input = int(input("Invalid choice.. Enter Rock(1) Paper(2) Scissors(3). Press (9) to quit: "))
 
-        match choice:
-            case 1:
-                choice = "Rock"
-            case 2:
-                choice = "Paper"
-            case 3:
-                choice = "Scissors"
-            case _:
-                choice = None
+        if user_input == 9:
+            return "Thanks for playing!"
 
-        
-        self.choice = choice
+        choices_map = {1: "Rock", 2: "Paper", 3: "Scissors"}
+        self.choice = choices_map.get(user_input, None)
 
 class Computer(Player):
+    def __init__(self, name):
+        super().__init__(name)
+        self.history = np.array([], dtype=str).reshape(0, 2)  # Initialize as an empty 2-column array
+
     def do_choice(self):
-        self.choice = random.choice(["Rock", "Paper", "Scissors"])
+        if self.history.size > 0:
+            self.choice = self.predict_choice()
+        else:
+            self.choice = random.choice(["Rock", "Paper", "Scissors"])
         print(f"{self.name} chose: {self.choice}")
+
+    def predict_choice(self):
+        player_choices = self.history[:, 0]
+        if player_choices.size > 0:
+            unique, counts = np.unique(player_choices, return_counts=True)
+            most_common_choice = unique[np.argmax(counts)]
+            counter_choices = {"Rock": "Paper", "Paper": "Scissors", "Scissors": "Rock"}
+            return counter_choices[most_common_choice]
+        else:
+            return random.choice(["Rock", "Paper", "Scissors"])
+
+    def record_choice(self, player_choice):
+        self.history = np.vstack([self.history, [player_choice, self.choice]])
